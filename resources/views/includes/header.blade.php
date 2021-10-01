d759c6c27ccaa242ad58fa6fd4f7113b9c88b39e
Forklein <giuseppepisani2@gmail.com>
2021-10-01 15:13:45 +0200

    first commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="{{route('home')}}">Home</a>
+                            <a href="{{route('comics')}}">Comics</a>
+                            <a href="{{route('characters')}}">Characters</a>
+                        </li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col">sss</div>
+            <div class="col">sss</div>
+        </div>
+    </div>
+</header>
