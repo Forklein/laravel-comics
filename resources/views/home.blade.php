da8ef80c8c3077766fda85ca59e1671e0f5e0eb6
Forklein <giuseppepisani2@gmail.com>
2021-10-01 21:16:42 +0200

    Add footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,16 +14,16 @@
                 @endforeach
             </div>
             <div class="row">
-                <div class="col">Load More</div>
+                <div class="col load">{{strtoupper('Load More')}}</div>
             </div>
         </div>
         <div class="bgc-primary">
             <div class="container">
-                <div class="row">
+                <div class="row p-3">
                     @foreach($icons as $icon)
                         <div class="icon d-flex">
                             <img src="{{asset('images/'.$icon['src'])}}" alt="{{$icon['name']}}">
-                            <p>{{$icon['name']}}</p>
+                            <p>{{strtoupper($icon['name'])}}</p>
                         </div>
                     @endforeach
                 </div>
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,65 @@
-<footer></footer>
+<footer>
+    <div class="faq">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="row p-3">
+                        <div class="faq-box">
+                            <h3>DC COMICS</h3>
+                            <ul>
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>Tv</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                            <h3>SHOP</h3>
+                            <ul>
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+                        </div>
+                        <div class="faq-box">
+                            <h3>DC</h3>
+                            <ul>
+                                <li>Terms Of Use</li>
+                                <li>Privacy policy (New)</li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+                        </div>
+                        <div class="faq-box">
+                            <h3>SITES</h3>
+                            <ul>
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row p-3">
+                        <p>
+                            All Site Content TM and © 2020 DC Entertainment, unless otherwise noted here. All rights reserved.
+                            Cookies Settings
+                        </p>
+                    </div>
+                </div>
+                <div class="col bg-footer"></div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row"></div>
+    </div>
+</footer>
