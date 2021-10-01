d3bccd7f9aae95ff968bd8ffbaf35ef0910ca887
Forklein <giuseppepisani2@gmail.com>
2021-10-01 16:39:59 +0200

    Add main layouts, include header and footer and set yield

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.main')
 
 @section('content')
-Test
+<div class="container">
+    <div class="row">
+        @foreach($cards as $key => $card)
+            <div class="col">
+                <p>{{$card['title']}}</p>
+            </div>
+        @endforeach
+    </div>
+</div>
 @endsection
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,21 +1,38 @@
 <header>
-    <div class="container">
+    <div class="top-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col d-flex">
+                    <p>DC POWER VISA ®</p>
+                    <p>ADDITIONAL DC SITES</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="menu container">
         <div class="row">
             <div class="col">
+                <img src="{{ asset('images/dc-logo.png')}}" alt="Logo">
+            </div>
+            <div class="col">
                 <nav>
-                    <ul>
-                        <li>
-                            <a href="{{route('home')}}">Home</a>
-                            <a href="{{route('comics')}}">Comics</a>
-                            <a href="{{route('characters')}}">Characters</a>
+                    <ul class="d-flex">
+                        <li class="active">
+                            <a href="{{route('home')}}">HOME</a>    
                         </li>
-                        <li></li>
-                        <li></li>
+                        <li>
+                            <a href="{{route('comics')}}">COMICS</a>
+                        </li>
+                        <li>
+                            <a href="{{route('characters')}}">CHARACTERS</a>
+                        </li>
                     </ul>
                 </nav>
             </div>
-            <div class="col">sss</div>
-            <div class="col">sss</div>
+            <div class="col">
+                <input type="text">
+            </div>
         </div>
     </div>
+    <div class="jumbotron"></div>
 </header>
