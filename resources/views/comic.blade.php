ba5ba58c00204be557e0e3059d7cb13d70517d80
Forklein <giuseppepisani2@gmail.com>
2021-10-04 21:17:03 +0200

    add more thing in comic blade

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -4,22 +4,30 @@
 @section('content')
 <main>
     <section id="comic">
-        <div class="bar bgc-primary"></div>
-        <div class="container">
+        <div class="bar bgc-primary">
+            <div class="container">
+                <div class="thumb">
+                    <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                </div>
+            </div>
+        </div>
+        <div class="container" style="margin-top: 25px">
             <p>Advertisement</p>
             <div class="row">
                 <div class="col">
                     <h1>{{$comic['title']}}</h1>
                     <div class="info d-flex">
                         <div class="info-price d-flex p-3">
-                            <p>{{$comic['price']}}</p>
-                            <p>Available</p>
+                            <p>U.S. Price: <span>{{$comic['price']}}</span></p>
+                            <p style="opacity:0.4">AVAILABLE</p>
                         </div>
                         <div class="info-availability p-3">
-                            <p>Check availability</p>
+                            <p>Check availability <i class="fas fa-sort-down"></i></p>
                         </div>
                     </div>
-                    <p>{{$comic['description']}}</p>
+                    <div class="description">
+                        <p>{{$comic['description']}}</p>
+                    </div>
                 </div>
                 <div class="col advertisement">
                     <img src="{{ asset('images/adv.jpg')}}" alt="advertisement">
